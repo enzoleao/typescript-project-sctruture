880148ff1fa0ef4ffb8ad9249dc9f6996639bc0b
Chrzddd <enzopinheiro6@gmail.com>
2023-05-07 19:55:39 -0300

    update last

--- src/server.ts
+++ src/server.ts
@@ -35,7 +35,7 @@ app.use(
   
       return response.status(500).json({
         status: "error",
-        message: `Internal server error - ${err.message}`,
+        message: `Internal server error. - ${err.message}`,
       });
     }
 );
